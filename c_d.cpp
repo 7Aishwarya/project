1fccf6dd798cf31c3b81eed969d14e65fd15aeeb
demo user <demouser@demo.com>
2018-03-22 12:05:54 +0530

    changes at lines in some commmon and uncommon areas

--- c_d.cpp
+++ c_d.cpp
@@ -11,7 +11,7 @@ abc(int a, int b)
 {
 cout<<"parameterised constructor\n";
 cout<<"two random nos: "<<a<<" and "<<b<<"\n";
-cout<<"changes done here\n";
+cout<<"changes done here , change in the common area\n";
 }
 ~abc()
 {
@@ -22,7 +22,7 @@ cout<<"second change done here\n";
 int main()
 {
 abc a1,a2(2,5);
-//a1=abc(2,5);
+cout<<"change in the are which is not common\n";
 return 0;
 }
  
