a5d0e1c7d770153215652e1b5a0d1ab53ef0d24b
demo user <demouser@demo.com>
2018-03-22 11:56:06 +0530

    changes at line 8, 14 for repititive codes

--- c_d.cpp
+++ c_d.cpp
@@ -5,13 +5,13 @@ class abc
 public:
 abc()
 {
-cout<<"dafault constructor\n";
+cout<<"dafault constructor   line 8\n";
 }
 abc(int a, int b)
 {
 cout<<"parameterised constructor\n";
 cout<<"two random nos: "<<a<<" and "<<b<<"\n";
-cout<<"changes done here\n";
+cout<<"changes done here   line 14\n";
 }
 ~abc()
 {
